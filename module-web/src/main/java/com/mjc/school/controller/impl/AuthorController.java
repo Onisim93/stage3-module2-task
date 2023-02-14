2b622843658939687df56db958b051cd305cf0e5
onisim_albarov <onisim93@gmail.com>
2023-02-14 18:43:42 +0400

    added annotation CommandHandler to module-web

--- module-web/src/main/java/com/mjc/school/controller/impl/AuthorController.java
+++ module-web/src/main/java/com/mjc/school/controller/impl/AuthorController.java
@@ -1,6 +1,7 @@
 package com.mjc.school.controller.impl;
 
 import com.mjc.school.controller.BaseController;
+import com.mjc.school.controller.command.annotation.CommandHandler;
 import com.mjc.school.service.BaseService;
 import com.mjc.school.service.dto.AuthorDto;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,26 +20,31 @@ public class AuthorController implements BaseController<AuthorDto, AuthorDto, Lo
     }
 
     @Override
+    @CommandHandler(nameOperation = "GET_ALL_AUTHORS")
     public List<AuthorDto> readAll() {
         return service.readAll();
     }
 
     @Override
+    @CommandHandler(nameOperation = "GET_AUTHOR_BY_ID")
     public AuthorDto readById(Long id) {
         return service.readById(id);
     }
 
     @Override
+    @CommandHandler(nameOperation = "CREATE_AUTHOR")
     public AuthorDto create(AuthorDto createRequest) {
         return service.create(createRequest);
     }
 
     @Override
+    @CommandHandler(nameOperation = "UPDATE_AUTHOR")
     public AuthorDto update(AuthorDto updateRequest) {
         return service.update(updateRequest);
     }
 
     @Override
+    @CommandHandler(nameOperation = "DELETE_AUTHOR")
     public boolean deleteById(Long id) {
         return service.deleteById(id);
     }
--- module-web/src/main/java/com/mjc/school/controller/impl/NewsController.java
+++ module-web/src/main/java/com/mjc/school/controller/impl/NewsController.java
@@ -1,6 +1,7 @@
 package com.mjc.school.controller.impl;
 
 import com.mjc.school.controller.BaseController;
+import com.mjc.school.controller.command.annotation.CommandHandler;
 import com.mjc.school.service.BaseService;
 import com.mjc.school.service.dto.NewsDto;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,26 +20,31 @@ public class NewsController implements BaseController<NewsDto, NewsDto, Long> {
     }
 
     @Override
+    @CommandHandler(nameOperation = "GET_ALL_NEWS")
     public List<NewsDto> readAll() {
         return service.readAll();
     }
 
     @Override
+    @CommandHandler(nameOperation = "GET_NEWS_BY_ID")
     public NewsDto readById(Long id) {
         return service.readById(id);
     }
 
     @Override
+    @CommandHandler(nameOperation = "CREATE_NEWS")
     public NewsDto create(NewsDto createRequest) {
         return service.create(createRequest);
     }
 
     @Override
+    @CommandHandler(nameOperation = "UPDATE_NEWS")
     public NewsDto update(NewsDto updateRequest) {
         return service.update(updateRequest);
     }
 
     @Override
+    @CommandHandler(nameOperation = "DELETE_NEWS")
     public boolean deleteById(Long id) {
         return service.deleteById(id);
     }
